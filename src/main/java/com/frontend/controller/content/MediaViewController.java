99b07b9654ed109265019c1cca6949724c124267
LeBonsBay <armin.keric0001@gmail.com>
2026-03-23 14:21:22 +0100

    feat: add of Media Controllers

---
+++ src/main/java/com/frontend/controller/content/MediaViewController.java
@@ -0,0 +1,13 @@
+package com.frontend.controller.content;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MediaViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
