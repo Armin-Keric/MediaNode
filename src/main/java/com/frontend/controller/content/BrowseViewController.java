4915a4c6bce64db739aaec141b13e43f0223b83e
Lukas <lradlspo@htl-steyr.ac.at>
2026-03-25 14:59:52 +0100

    feat: first implementation of the browse tab

--- src/main/java/com/frontend/controller/content/BrowseViewController.java
+++ src/main/java/com/frontend/controller/content/BrowseViewController.java
@@ -1,8 +1,12 @@
 package com.frontend.controller.content;
 
+import com.frontend.MainController;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,21 +14,44 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class BrowseViewController implements Initializable {
+public class BrowseViewController extends MainController implements Initializable {
     public TextField searchTextField;
     public ComboBox<String> mediaTypeComboBox;
     public ComboBox<String> mediaGenreComboBox;
     public ComboBox<String> mediaYearComboBox;
+    public HBox featuredAreaVBox;
+    public HBox friendAreaVBox;
+    public GridPane browseAreaVBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // add combobox options
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/com/frontend/data/browse-filteroptions.csv"))) {
-            System.out.println(reader.readLine());
-            System.out.println(reader.readLine());
+        int OBJECTS_PER_ROW = 5;
+        int OBJECTS_PER_COL = 5;
 
+        // add combobox options (year is tmp)
+        mediaYearComboBox.getItems().addAll("ASC", "DESC");
+        // .getClass().getResources(""); doesn't work?
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/com/frontend/data/browse-filteroptions.csv"))) {
+            mediaTypeComboBox.getItems().addAll(reader.readLine().split(";"));
+            mediaGenreComboBox.getItems().addAll(reader.readLine().split(";"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        featuredAreaVBox.getChildren().addAll(
+                new AnchorPane(),
+                new AnchorPane(),
+                new AnchorPane(),
+                new AnchorPane(),
+                new AnchorPane()
+        );
+
+        loadView((AnchorPane) featuredAreaVBox.getChildren().get(0), "media-embed-view.fxml", "BrowseController");
+        loadView((AnchorPane) featuredAreaVBox.getChildren().get(1), "media-embed-view.fxml", "BrowseController");
+        loadView((AnchorPane) featuredAreaVBox.getChildren().get(2), "media-embed-view.fxml", "BrowseController");
+        loadView((AnchorPane) featuredAreaVBox.getChildren().get(3), "media-embed-view.fxml", "BrowseController");
+        loadView((AnchorPane) featuredAreaVBox.getChildren().get(4), "media-embed-view.fxml", "BrowseController");
     }
+
+
 }
