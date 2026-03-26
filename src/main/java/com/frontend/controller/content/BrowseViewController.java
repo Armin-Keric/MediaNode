8135dce0603d2cf1db28a50e73dc9c1d7e83e166
LeBonsBay <armin.keric0001@gmail.com>
2026-03-26 10:29:41 +0100

    -

--- src/main/java/com/frontend/controller/content/BrowseViewController.java
+++ src/main/java/com/frontend/controller/content/BrowseViewController.java
@@ -1,5 +1,6 @@
 package com.frontend.controller.content;
 
+import com.backend.model.Genres;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -8,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class BrowseViewController implements Initializable {
@@ -18,13 +21,10 @@ public class BrowseViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // add combobox options
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/com/frontend/data/browse-filteroptions.csv"))) {
-            System.out.println(reader.readLine());
-            System.out.println(reader.readLine());
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        try {
+            mediaGenreComboBox.getItems().addAll(Collections.singletonList(Genres.getGenres()));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
