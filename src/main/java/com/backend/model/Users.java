dac08313d1807d077937ca47ba7bb07e3868eb50
LeBonsBay <armin.keric0001@gmail.com>
2026-03-28 14:39:11 +0100

    feat: add of some getters in the BrowseViewController class and filled the dropboxes with data

--- src/main/java/com/backend/model/Users.java
+++ src/main/java/com/backend/model/Users.java
@@ -1,5 +1,37 @@
 package com.backend.model;
 
 
-public record Users(Integer user_id,String name, String password, String biography, String img_url) {
+import com.backend.Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public record Users(Integer user_id, String name, String password, String biography, String img_url) {
+
+    public static List<Users> getUsers() throws SQLException {
+        Database database = Database.getInstance();
+        List<Users> results = new ArrayList<>();
+        Connection c = database.getConnection();
+        Statement stmt = c.createStatement();
+        ResultSet res = stmt.executeQuery("SELECT * FROM users");
+
+        while (res.next()) {
+            Users u = new Users(
+                    res.getInt("user_id"),
+                    res.getString("name"),
+                    res.getString("password"),
+                    res.getString("biography"),
+                    res.getString("img_url")
+                    );
+            results.add(u);
+        }
+
+        return results;
+    }
+
+
 }
--- src/main/java/com/frontend/controller/content/BrowseViewController.java
+++ src/main/java/com/frontend/controller/content/BrowseViewController.java
@@ -1,5 +1,7 @@
 package com.frontend.controller.content;
 
+import com.backend.model.Genres;
+import com.backend.model.Media;
 import com.frontend.MainController;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -8,36 +10,39 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class BrowseViewController extends MainController implements Initializable {
     public TextField searchTextField;
     public ComboBox<String> mediaTypeComboBox;
-    public ComboBox<String> mediaGenreComboBox;
+    public ComboBox<Genres> mediaGenreComboBox;
     public ComboBox<String> mediaYearComboBox;
     public HBox featuredAreaVBox;
     public HBox friendAreaVBox;
     public GridPane browseAreaVBox;
+    private Media m;
+    private List<String> typeOfMedias = new ArrayList<>();
+    private String[] mediatypes;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         int OBJECTS_PER_ROW = 5;
         int OBJECTS_PER_COL = 5;
-
+        mediatypes = new String[]{"Anime", "Game", "Music", "Movie", "TVshow"};
+        typeOfMedias.addAll(List.of(mediatypes));
         // add combobox options (year is tmp)
-        mediaYearComboBox.getItems().addAll("ASC", "DESC");
         // .getClass().getResources(""); doesn't work?
-        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/com/frontend/data/browse-filteroptions.csv"))) {
-            mediaTypeComboBox.getItems().addAll(reader.readLine().split(";"));
-            mediaGenreComboBox.getItems().addAll(reader.readLine().split(";"));
-        } catch (IOException e) {
-            e.printStackTrace();
+        try {
+            mediaGenreComboBox.getItems().addAll(Genres.getGenres());
+            mediaYearComboBox.getItems().addAll("ASC", "DESC");
+            mediaTypeComboBox.getItems().addAll(typeOfMedias);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
         for (int i = 0; i < OBJECTS_PER_ROW; ++i) {
@@ -47,5 +52,23 @@ public class BrowseViewController extends MainController implements Initializabl
             loadView((AnchorPane) featuredAreaVBox.getChildren().get(i), "media-embed-view.fxml", "BrowseController");
             loadView((AnchorPane) friendAreaVBox.getChildren().get(i), "media-embed-view.fxml", "BrowseController");
         }
+    }
+
+    public String getSelectedTypeOfMedia(javafx.event.ActionEvent actionEvent) {
+        String target = mediaTypeComboBox.getSelectionModel().getSelectedItem();
+        System.out.println(target);
+        return target;
+    }
+
+    public String getSelectedGenre(javafx.event.ActionEvent actionEvent) {
+        String target = String.valueOf(mediaGenreComboBox.getSelectionModel().getSelectedItem());
+        System.out.println(target);
+        return target;
+    }
+
+    public String getSelectedYear(){
+        String target = mediaYearComboBox.getSelectionModel().getSelectedItem();
+        System.out.println(target);
+        return target;
     }
 }
