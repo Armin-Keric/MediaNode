d7fcf93a377a583e16f1a17fc0d40f7058047cb0
LeBonsBay <armin.keric0001@gmail.com>
2026-03-30 19:25:45 +0200

    feat: implementation of filtering in the Browse View by genre, year etc...

--- src/main/java/com/frontend/controller/content/BrowseViewController.java
+++ src/main/java/com/frontend/controller/content/BrowseViewController.java
@@ -2,6 +2,7 @@ package com.frontend.controller.content;
 
 import com.backend.model.Genres;
 import com.backend.model.Media;
+import com.backend.model.User_library;
 import com.frontend.MainController;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -11,6 +12,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -29,12 +32,19 @@ public class BrowseViewController extends MainController implements Initializabl
         int OBJECTS_PER_ROW = 5;
         int OBJECTS_PER_COL = 5;
         // tmp
-        String[] mediaTypes = new String[]{"Anime", "Game", "Music", "Movie", "TVshow"};
+        String[] mediaTypes = new String[]{"ALL", "Anime", "Game", "Music", "Movie", "TVshow"};
 
         try {
+            Genres fakeGenre = new Genres(null, "ALL");
+            mediaGenreComboBox.getItems().add(fakeGenre);
             mediaGenreComboBox.getItems().addAll(Genres.getGenres());
+            mediaGenreComboBox.setValue(mediaGenreComboBox.getItems().getFirst());
+
             mediaYearComboBox.getItems().addAll("ASC", "DESC");
+            mediaYearComboBox.setValue(mediaYearComboBox.getItems().getFirst());
+
             mediaTypeComboBox.getItems().addAll(List.of(mediaTypes));
+            mediaTypeComboBox.setValue(mediaTypeComboBox.getItems().getFirst());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -46,26 +56,49 @@ public class BrowseViewController extends MainController implements Initializabl
             MediaViewController tmpFeatured = (MediaViewController) loadView((AnchorPane) featuredAreaVBox.getChildren().get(i), "media-embed-view.fxml", "BrowseController");
             MediaViewController tmpFriends = (MediaViewController) loadView((AnchorPane) friendAreaVBox.getChildren().get(i), "media-embed-view.fxml", "BrowseController");
 
-            tmpFeatured.setMedia("https://upload.wikimedia.org/wikipedia/commons/d/d3/Kiwi_aka.jpg", "Kiwi", "10/10");
-            tmpFriends.setMedia("https://upload.wikimedia.org/wikipedia/commons/d/d3/Kiwi_aka.jpg", "Kas", "10/10");
+            try {
+
+                tmpFeatured.setMedia(Media.medias_type("Game").get(i).getImg_url(), Media.medias_type("Game").get(i).getTitle(), "10/10");
+
+                //the media shown should always be sorted by date at the start...
+                tmpFriends.setMedia(Media.medias("ASC").get(i).getImg_url(), Media.medias("ASC").get(i).getTitle(), "10/10");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    public String getSelectedTypeOfMedia(javafx.event.ActionEvent actionEvent) {
-        String target = mediaTypeComboBox.getSelectionModel().getSelectedItem();
-        System.out.println(target);
-        return target;
+    public void updateLists() throws SQLException {
+        int OBJECTS_PER_ROW = 5;
+        String typeOfMedia = mediaTypeComboBox.getSelectionModel().getSelectedItem();
+        String genre = String.valueOf(mediaGenreComboBox.getSelectionModel().getSelectedItem());
+        String year = mediaYearComboBox.getSelectionModel().getSelectedItem();
+
+
+        friendAreaVBox.getChildren().clear();
+        List<Media> currentData = Media.getCurrentData(typeOfMedia, genre, year);
+
+
+        for (int i = 0; i < OBJECTS_PER_ROW; ++i) {
+            featuredAreaVBox.getChildren().add(new AnchorPane());
+            friendAreaVBox.getChildren().add(new AnchorPane());
+
+            MediaViewController tmpFriends = (MediaViewController) loadView((AnchorPane) friendAreaVBox.getChildren().get(i), "media-embed-view.fxml", "BrowseController");
+            tmpFriends.setMedia(currentData.get(i).getImg_url(), currentData.get(i).getTitle(), "10/10");
+
+            //tmpFriends.setMedia(,,);
+        }
     }
 
-    public String getSelectedGenre(javafx.event.ActionEvent actionEvent) {
-        String target = String.valueOf(mediaGenreComboBox.getSelectionModel().getSelectedItem());
-        System.out.println(target);
-        return target;
+    public void getSelectedTypeOfMedia(javafx.event.ActionEvent actionEvent) throws SQLException {
+        updateLists();
     }
 
-    public String getSelectedYear(){
-        String target = mediaYearComboBox.getSelectionModel().getSelectedItem();
-        System.out.println(target);
-        return target;
+    public void getSelectedGenre(javafx.event.ActionEvent actionEvent) throws SQLException {
+        updateLists();
+    }
+
+    public void getSelectedYear() throws SQLException {
+        updateLists();
     }
 }
