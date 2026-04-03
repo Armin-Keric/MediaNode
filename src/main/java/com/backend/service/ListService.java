5f039cbb833c204e2bce15a8b651cda1c8818d9f
LeBonsBay <armin.keric0001@gmail.com>
2026-04-03 15:18:45 +0200

    feat: implementation of BarChart and activity log

--- src/main/java/com/backend/service/ListService.java
+++ src/main/java/com/backend/service/ListService.java
@@ -1,10 +1,16 @@
 package com.backend.service;
 
 import com.backend.Database;
+import com.backend.model.Media;
+import javafx.scene.chart.XYChart;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListService {
 
@@ -26,5 +32,47 @@ public class ListService {
             stmt.setInt(4, rating);
         }
         stmt.executeUpdate();
+    }
+
+    public static List<String> getActivityLog(int sessionId) throws SQLException {
+        Database database = Database.getInstance();
+        Connection c = database.getConnection();
+        List<String> result = new ArrayList<>();
+
+        String sql = "SELECT m.title, ul.status, ul.score, ul.created_at FROM media m JOIN user_library ul USING(id)" +
+                " WHERE ul.user_id = ? ORDER BY ul.created_at DESC";
+
+        PreparedStatement stmt = c.prepareStatement(sql);
+        stmt.setInt(1, AuthService.sessionId);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            result.add(rs.getString("title") + " | " + rs.getString("status") + " | " + rs.getObject("created_at"));
+        }
+
+        return result;
+    }
+
+    //basically for building the Chart in the Profile
+    public static XYChart.Series<String, Number> getRatings() throws SQLException {
+        Database database = Database.getInstance();
+        Connection c = database.getConnection();
+        XYChart.Series<String,Number> result = new XYChart.Series<>();
+
+        String sql = "SELECT score, COUNT(*) AS total FROM user_library WHERE user_id = ? AND score > 0 GROUP BY score ORDER BY score ASC";
+
+        PreparedStatement stmt = c.prepareStatement(sql);
+        stmt.setInt(1, AuthService.sessionId);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            String value = String.valueOf(rs.getInt("score"));
+            int total = rs.getInt("total");
+            result.getData().add(new XYChart.Data<>(value,total));
+        }
+
+        return result;
     }
 }
--- src/main/java/com/frontend/controller/content/ProfileViewController.java
+++ src/main/java/com/frontend/controller/content/ProfileViewController.java
@@ -1,12 +1,15 @@
 package com.frontend.controller.content;
 
+import com.backend.model.Media;
 import com.backend.model.Users;
 import com.backend.service.AuthService;
+import com.backend.service.ListService;
 import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
@@ -23,33 +26,41 @@ public class ProfileViewController implements Initializable {
     public Label biographyLabel;
     public GridPane favMediaGridPane;
     public BarChart userGraphChart;
-    public ListView overViewListView;
+    public ListView<String> overViewListView;
     //public AnchorPane contentPane;
     //public AuthService authService = new AuthService();
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        CategoryAxis categoryAxis = new CategoryAxis();
-
-        categoryAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")));
-        categoryAxis.setLabel("Ratings");
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Total");
-
-        if(AuthService.sessionId != 0){
-            try {
-                userNameLabel.setText(setUserName());
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+        try {
+            loadBarChart();
+            overViewListView.getItems().clear();
+            if (AuthService.sessionId != 0) {
+                try {
+                    userNameLabel.setText(setUserName());
+                    overViewListView.getItems().setAll(ListService.getActivityLog(AuthService.sessionId));
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     //sets the username
-   public String setUserName() throws SQLException {
+    public String setUserName() throws SQLException {
         return Users.getUser();
-   }
+    }
+
+    public void loadBarChart() throws SQLException {
+        NumberAxis yAxis = (NumberAxis) userGraphChart.getYAxis();
+        //yAxis.setTickUnit(1); seems like this doesn't work?
+        //yAxis.setMinorTickVisible(false);
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        userGraphChart.getData().clear();
+        userGraphChart.getData().add(ListService.getRatings());
+    }
 }
