4ca1be11d3faea28813f031aa09cb1f1f2b84f50
LeBonsBay <armin.keric0001@gmail.com>
2026-04-23 13:24:06 +0200

    chore: add of .env stuff

--- src/main/java/com/backend/Database.java
+++ src/main/java/com/backend/Database.java
@@ -1,18 +1,31 @@
 package com.backend;
 
 
-import java.sql.*;
+import io.github.cdimascio.dotenv.Dotenv;
 
+import java.sql.*;
 
 
 public class Database {
     private static Database instance;
     Connection connection;
+    private static final Dotenv dotenv = Dotenv.load(); //loads .env file
+    private final String USER = dotenv.get("POSTGRES_USER");
+    private final String PASSWORD = dotenv.get("POSTGRES_PASSWORD");
+    private final String DB_NAME = dotenv.get("POSTGRES_DB");
 
     private Database() throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/media_collection?" + "user=user&password=password";
+        String url = "jdbc:postgresql://localhost:5432/" + DB_NAME + "?user=" + USER  + "&password=" + PASSWORD;
         connection = DriverManager.getConnection(url);
     }
+
+    /**
+     * POSTGRES_PASSWORD: password
+     * POSTGRES_DB: media_collection
+     * POSTGRES_USER: user
+     * @return
+     * @throws SQLException
+     */
 
     public static Database getInstance() throws SQLException {
         if (instance == null) {
